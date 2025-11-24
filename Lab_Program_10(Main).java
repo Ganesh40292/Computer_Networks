742e16df5c3c5c37c3428728b496f259a7e88710
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-24 10:42:57 +0530

    Update Lab_Program_10(Main).java

--- Lab_Program_10(Main).java
+++ Lab_Program_10(Main).java
@@ -1,69 +1,81 @@
-
 import java.util.Scanner;
+
 public class Leaky
 {
-    public static void main(String []args)
+    public static void main(String[] args)
     {
         int i;
-        int a[]=new int [20];
-        int buck_rem=0,sent,recv;
-        Scanner in=new Scanner(System.in);
+        int a[] = new int[20];
+        int buck_rem = 0, sent, recv;
+
+        Scanner in = new Scanner(System.in);
+
         System.out.println("enter the number of packets:");
-        int n=in.nextInt();
+        int n = in.nextInt();
+
         System.out.println("enter the buckets capacity:");
-        int buck_cap=in.nextInt();
-         System.out.println("enter the output rate:");
-        int rate=in.nextInt();
+        int buck_cap = in.nextInt();
+
+        System.out.println("enter the output rate:");
+        int rate = in.nextInt();
+
         System.out.println("enter the size of packets:");
-        for(i=1;i<=n;i++)
+        for(i = 0; i < n; i++)
         {
-            a[i]=in.nextInt();
+            a[i] = in.nextInt();
         }
-        System.out.println("CLOCK\t PACKTES SIZE\t ACCEPT\t SENT\t REMAINING");
-        for(i=1;i<=n;i++)
+
+        System.out.println("CLOCK\tPACKET_SIZE\tACCEPT\tSENT\tREMAINING");
+
+        for(i = 0; i < n; i++)
         {
-            if(a[i]!=0)
+            int dropped = 0;
+
+            if(a[i] != 0)
             {
-                if(buck_rem+a[i]>buck_cap)
+                if(buck_rem + a[i] > buck_cap)
                 {
-                    recv=buck_cap-buck_rem;
-                    buck_rem=buck_cap;
+                    recv = buck_cap - buck_rem;
+                    dropped = (buck_rem + a[i]) - buck_cap;
+                    buck_rem = buck_cap;
                 }
                 else
                 {
-                    recv=a[i];
-                    buck_rem+=a[i];
+                    recv = a[i];
+                    buck_rem += a[i];
                 }
             }
             else
             {
-                recv=0;
+                recv = 0;
             }
-            if(buck_rem!=0)
+
+            if(buck_rem != 0)
             {
-                if(buck_rem<rate)
+                if(buck_rem < rate)
                 {
-                    sent=buck_rem;
-                    buck_rem=0;
+                    sent = buck_rem;
+                    buck_rem = 0;
                 }
                 else
                 {
-                    sent=rate;
-                    buck_rem=buck_rem-rate;
+                    sent = rate;
+                    buck_rem -= rate;
                 }
             }
             else
             {
-                sent=0;
+                sent = 0;
             }
-            if(recv==-1)
+
+            if(dropped > 0)
             {
-                System.out.println(i+"\t\t"+a[i]+"\t dropped\t"+sent+"\t"+buck_rem);
+                System.out.println((i+1) + "\t\t" + a[i] + "\t\t" + recv + " (dropped:" + dropped + ")\t" + sent + "\t" + buck_rem);
             }
             else
             {
-                System.out.println(i+"\t\t"+a[i]+"\t\t"+recv+"\t"+sent+"\t"+buck_rem);
+                System.out.println((i+1) + "\t\t" + a[i] + "\t\t" + recv + "\t\t" + sent + "\t" + buck_rem);
             }
-        }        
-    }   
+        }
+    }
 }
