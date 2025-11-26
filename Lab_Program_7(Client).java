451fe1d25263364a6d433aabe67b9d3b000a404e
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-26 21:59:08 +0530

    Update Lab_Program_7(Client).java

--- Lab_Program_7(Client).java
+++ Lab_Program_7(Client).java
@@ -1,32 +1,39 @@
 package client;
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class Client 
+public class Client
 {
-    public static void main(String args[])throws Exception
+    public static void main(String args[]) throws Exception
     {
-        Scanner in=new Scanner(System.in);
-        Socket ClientSocket=new Socket("127.0.01",4000);
-        System.out.println("***Client side***");
-        
-        System.out.println("Enter the filename to transfer:");
-        String fname=in.nextLine();
-        
-        OutputStream ostream=ClientSocket.getOutputStream();
-        PrintWriter pwrite=new PrintWriter(ostream,true);
+        Scanner in = new Scanner(System.in);
+
+        Socket ClientSocket = new Socket("127.0.0.1", 4000);
+
+        System.out.println("*** Client side ***");
+
+        System.out.print("Enter the filename to transfer: ");
+        String fname = in.nextLine();
+
+        OutputStream ostream = ClientSocket.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
         pwrite.println(fname);
-        
-        InputStream istream=ClientSocket.getInputStream();
-        BufferedReader SocketRead=new BufferedReader(new InputStreamReader(istream));
-        System.out.println("Contents of the file "+ fname +" are");
+
+        InputStream istream = ClientSocket.getInputStream();
+        BufferedReader SocketRead = new BufferedReader(new InputStreamReader(istream));
+
+        System.out.println("Contents of the file " + fname + " are:");
+
         String str;
-        while((str=SocketRead.readLine())!=null)
+        while ((str = SocketRead.readLine()) != null)
         {
             System.out.println(str);
         }
+
         pwrite.close();
         SocketRead.close();
+        ClientSocket.close();
     }
 }
