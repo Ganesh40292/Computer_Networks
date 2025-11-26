d19b61c7bd5e42c65a8f21e166327ae495f4fe47
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-26 20:00:25 +0530

    Update Lab_Program_9(Main).java

--- Lab_Program_9(Main).java
+++ Lab_Program_9(Main).java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class Program9 
+
+public class Program9
 {
-    public static void main(String[] args) 
-  {
+    public static void main(String[] args)
+    {
         String msg;
         int pt[] = new int[100];
         int ct[] = new int[100];
@@ -10,13 +11,13 @@ public class Program9
 
         Scanner in = new Scanner(System.in);
 
-        // Input two primes p and q until both are prime
-        do 
+        do
         {
             System.out.println("Enter the two large prime numbers for p and q:");
             p = in.nextInt();
             q = in.nextInt();
-        } while (prime(p) == 0 || prime(q) == 0);
+        }
+        while (prime(p) == 0 || prime(q) == 0);
 
         n = p * q;
         z = (p - 1) * (q - 1);
@@ -24,8 +25,7 @@ public class Program9
         System.out.println("n = " + n);
         System.out.println("z = " + z);
 
-        // Find e such that gcd(e, z) == 1
-        for (e = 2; e < z; e++) 
+        for (e = 2; e < z; e++)
         {
             if (gcd(e, z) == 1)
                 break;
@@ -34,8 +34,7 @@ public class Program9
         System.out.println("e = " + e);
         System.out.println("Public Key: (" + e + ", " + n + ")");
 
-        // Find d such that (d*e) % z == 1
-        for (d = 2; d < z; d++) 
+        for (d = 2; d < z; d++)
         {
             if ((e * d) % z == 1)
                 break;
@@ -44,38 +43,42 @@ public class Program9
         System.out.println("d = " + d);
         System.out.println("Private Key: (" + d + ", " + n + ")");
 
-        in.nextLine();  // consume leftover newline
+        in.nextLine();
 
         System.out.println("Enter the message for Encryption:");
         msg = in.nextLine();
 
         mlen = msg.length();
 
-        // Convert message chars to ASCII int array
         for (int i = 0; i < mlen; i++)
         {
             pt[i] = msg.charAt(i);
         }
+
         System.out.println("ASCII values of plaintext:");
         for (int i = 0; i < mlen; i++)
         {
             System.out.println(pt[i]);
         }
+
         System.out.println("Encryption: Cipher Text Obtained");
         for (int i = 0; i < mlen; i++)
         {
             ct[i] = mult(pt[i], e, n);
-        }  
+        }
+
         for (int i = 0; i < mlen; i++)
         {
             System.out.print(ct[i] + "\t");
         }
+
         System.out.println("\nDecryption: Plain Text Obtained");
         for (int i = 0; i < mlen; i++)
         {
             pt[i] = mult(ct[i], d, n);
         }
-        for (int i = 0; i < mlen; i++) 
+
+        for (int i = 0; i < mlen; i++)
         {
             System.out.println(pt[i] + " : " + (char) pt[i]);
         }
@@ -83,9 +86,9 @@ public class Program9
         in.close();
     }
 
-    // Compute gcd of a and b
-    public static int gcd(int a, int b) {
-        while (b != 0) 
+    public static int gcd(int a, int b)
+    {
+        while (b != 0)
         {
             int temp = b;
             b = a % b;
@@ -94,12 +97,12 @@ public class Program9
         return a;
     }
 
-    // Check if num is prime; returns 1 if prime, else 0
-    public static int prime(int num) 
+    public static int prime(int num)
     {
         if (num <= 1)
             return 0;
-        for (int i = 2; i <= Math.sqrt(num); i++) 
+
+        for (int i = 2; i <= Math.sqrt(num); i++)
         {
             if (num % i == 0)
                 return 0;
@@ -107,10 +110,10 @@ public class Program9
         return 1;
     }
 
-    // Compute (base^exp) % n using repeated multiplication (not efficient)
-    public static int mult(int base, int exp, int n) 
+    public static int mult(int base, int exp, int n)
     {
         int res = 1;
+
         for (int j = 1; j <= exp; j++)
         {
             res = (res * base) % n;
