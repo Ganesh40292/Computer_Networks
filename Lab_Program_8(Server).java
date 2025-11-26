3902c9ef2391c3f4b8bf92eda3e75b5e1512b2cf
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-26 23:24:54 +0530

    Update Lab_Program_8(Server).java

--- Lab_Program_8(Server).java
+++ Lab_Program_8(Server).java
@@ -1,40 +1,44 @@
 package datagram_server;
 
 import java.net.*;
-
 import java.util.*;
 
-public class Datagram_Server 
+public class Datagram_Server
 {
-
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args) throws Exception
     {
         Scanner sc = new Scanner(System.in);
 
         DatagramSocket serverSocket = new DatagramSocket(9000);
-
         byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
-        System.out.println("---Server side---");
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        byte[] sendData;
+
+        System.out.println("****** SERVER TERMINAL ******");
+
+        DatagramPacket receivePacket =
+                new DatagramPacket(receiveData, receiveData.length);
+
         serverSocket.receive(receivePacket);
-        System.out.println(new String(receivePacket.getData()));
+
+        String clientMsg =
+            new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+        System.out.println("Client connected: " + clientMsg);
+
         InetAddress IPAddress = receivePacket.getAddress();
         int port = receivePacket.getPort();
-        while (true) 
+
+        while (true)
         {
-            System.out.println("Type some message to display to client end");
+            System.out.print("Server message: ");
             String message = sc.nextLine();
+
             sendData = message.getBytes();
 
-            System.out.println("Message sent from the server: " + new String(sendData));
-
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            DatagramPacket sendPacket =
+                new DatagramPacket(sendData, sendData.length, IPAddress, port);
 
             serverSocket.send(sendPacket);
-
         }
-
     }
-
 }
