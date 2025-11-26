1b3d99efed64e17b583e8724712277e7496804eb
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-26 23:24:23 +0530

    Update Lab_Program_8(Client).java

--- Lab_Program_8(Client).java
+++ Lab_Program_8(Client).java
@@ -1,28 +1,37 @@
 package datagram_client;
+
 import java.net.*;
- public class Datagram_Client 
+
+public class Datagram_Client
 {
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args) throws Exception
     {
         String line = "connected with client";
+
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
+
+        byte[] sendData = line.getBytes();
         byte[] receiveData = new byte[1024];
-        sendData = line.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9000);
+
+        DatagramPacket sendPacket =
+                new DatagramPacket(sendData, sendData.length, IPAddress, 9000);
 
         clientSocket.send(sendPacket);
-        System.out.println("******client display terminal******");
-        while (true) 
+
+        System.out.println("****** CLIENT TERMINAL ******");
+
+        while (true)
         {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            DatagramPacket receivePacket =
+                    new DatagramPacket(receiveData, receiveData.length);
+
             clientSocket.receive(receivePacket);
-            String messageReceived = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
 
-            System.out.println("Message received from server: " + messageReceived);
+            String message =
+                new String(receivePacket.getData(), 0, receivePacket.getLength());
 
+            System.out.println("Server: " + message);
         }
-
     }
 }
