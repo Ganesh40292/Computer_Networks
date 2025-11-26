9a419a385f263cc821bc8e323e82df054de18d84
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-11-27 00:19:20 +0530

    Update Lab_Program_5.java

--- Lab_Program_5.java
+++ Lab_Program_5.java
@@ -1,52 +1,61 @@
 package swp;
-import java.util.Arrays;
-public class SWP 
+
+public class SWP
 {
     private int windowSize;
     private int[] frames;
     private boolean[] ack;
-    public SWP(int windowSize,int frameCount)
+
+    public SWP(int windowSize, int frameCount)
     {
-        this.windowSize=windowSize;
-        this.frames=new int[frameCount];
-        this.ack=new boolean[frameCount];
-        for(int i=0;i<frameCount;i++)
+        this.windowSize = windowSize;
+        this.frames = new int[frameCount];
+        this.ack = new boolean[frameCount];
+
+        for (int i = 0; i < frameCount; i++)
         {
-            frames[i]=i;
-            ack[i]=false;
+            frames[i] = i;
+            ack[i] = false;
         }
     }
+
     public void sendFrames()
     {
-        int sendIndex=0;
-        while(sendIndex<frames.length)
+        int sendIndex = 0;
+
+        while (sendIndex < frames.length)
         {
-            for(int i=0;i<windowSize&&(sendIndex+i)<frames.length;i++)
+            System.out.println("\nWindow starting at frame: " + sendIndex);
+
+            for (int i = 0; i < windowSize && (sendIndex + i) < frames.length; i++)
             {
-                System.out.println("sending frame:"+frames[sendIndex+i]);
+                System.out.println("Sending frame: " + frames[sendIndex + i]);
             }
-            for(int i=0;i<windowSize&&(sendIndex+i)<frames.length;i++)
+
+            for (int i = 0; i < windowSize && (sendIndex + i) < frames.length; i++)
             {
-                ack[sendIndex+i]=reciveAck(sendIndex+i);
+                ack[sendIndex + i] = receiveAck(sendIndex + i);
             }
-            while(sendIndex<frames.length&&ack[sendIndex])
+
+            while (sendIndex < frames.length && ack[sendIndex])
             {
                 sendIndex++;
             }
         }
     }
-    private boolean reciveAck(int frame)
+
+    private boolean receiveAck(int frame)
     {
-        System.out.println("receiving ack for frame:"+frame);
+        System.out.println("ACK received for frame: " + frame);
         return true;
     }
-    public static void main(String[] args) 
+
+    public static void main(String[] args)
     {
-        int windowSize=4;
-        int frameCount=10;
-        SWP swp =new SWP(windowSize,frameCount);
+        int windowSize = 4;
+        int frameCount = 10;
+
+        SWP swp = new SWP(windowSize, frameCount);
         swp.sendFrames();
     }
 }
-    
-
